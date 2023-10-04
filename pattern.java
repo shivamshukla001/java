d0dfe953c920d586678db592a29c197225fec24e
Shivamshukla <114076309+shivamshukla001@users.noreply.github.com>
2023-10-04 04:10:16 +0000

    pattern_01 in java

---
+++ pattern.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class pattern {
+       
+
+    public static void main(String[] args) {
+        
+        // for(int line=1; line<=4; line++){
+        //     for(int star=1; star<=line; star++){
+        //          System.out.print("*");
+        //     }
+        //    System.out.println();
+        // }
+         
+         
+        //inverted star pattern
+        // Scanner sc=new Scanner(System.in);
+        // int n=sc.nextInt();
+
+        // for(int lines=1; lines<=n; lines++){
+        //     for(int star=1; star<=n-lines+1; star++){
+        //              System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+       
+        //  int n=4;
+        // for(int lines=1; lines<=n; lines++){
+        //     for(int number=1; number<=lines; number++){
+        //        System.out.print(number);
+        //     }
+        //     System.out.println();
+        // }
+
+        int n=4;
+        char ch='A';
+        for(int lines=1; lines<=n; lines++){
+            for(int chara=1; chara<=lines; chara++){
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+
+
+
+        
+    }
+}
+ 
